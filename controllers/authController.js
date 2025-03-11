0e54f472bd2dfef5104142ba4bd9732ef8960ae3
laingoc99 <laingoc99@gmail.com>
2025-03-11 22:55:10 +0700

    update toi nay ne

--- controllers/authController.js
+++ controllers/authController.js
@@ -34,3 +34,29 @@ exports.login = async (req, res) => {
         res.status(500).json({ message: 'Lỗi server', error });
     }
 };
+
+exports.getMe = async (req, res) => {
+    try {
+        const authHeader = req.headers.authorization;
+        if (!authHeader || !authHeader.startsWith('Bearer ')) {
+            return res.status(401).json({ message: 'Không có token hoặc token không hợp lệ' });
+        }
+        const token = authHeader.split(' ')[1];
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const userId = decoded.id;
+
+        console.log("Giá trị userId", userId);
+        
+        const user = await User.findOne({ where: { id_user: userId } });
+        if (!user) return res.status(404).json({ message: 'Người dùng không tồn tại!' });
+
+        res.status(200).json({
+            user_id: user.id_user,
+            email: user.email,
+            role: user.role
+        });
+    } catch (error) {
+        res.status(401).json({ message: 'Token không hợp lệ hoặc hết hạn', error });
+    }
+};
